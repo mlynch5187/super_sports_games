bb0e0589cf2076a45198b27024375d58a19fdbb4
Alex Lynch <mlynch5187@gmail.com>
2020-01-30 13:48:03 -0700

    Complete standard_deviation

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,5 +1,23 @@
 ages = [24, 30, 18, 20, 41]
 
-# Your code here for calculating the standard deviation
+sum_of_integers = ages.sum
 
-# When you find the standard deviation, print it out
+number_of_integers = ages.length
+
+average_of_integers = (sum_of_integers.to_f / number_of_integers)
+
+subtracted = ages.map do |age|
+                age - average_of_integers
+              end
+
+squared = subtracted.map do |subtracted_age|
+            subtracted_age ** 2
+          end
+
+squared_sum = squared.sum
+
+divided_sum = squared_sum / number_of_integers
+
+square_root = Math.sqrt(divided_sum).round(2)
+
+p square_root
